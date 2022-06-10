5f84caab8e2fe785c186fa96e78efb859ca925bf
IvanPliska <94367970+IvanPliska@users.noreply.github.com>
2022-06-10 15:41:26 +0300

    Correct_2_Homework_5_1

--- src/test/java/ru/netology/sqr/Homework_5_1/calculation/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/Homework_5_1/calculation/SQRServiceTest.java
@@ -3,9 +3,8 @@ package ru.netology.sqr.Homework_5_1.calculation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
-public class SQRServicetest {
+public class SQRServiceTest {
 
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/SQRServ.csv")
